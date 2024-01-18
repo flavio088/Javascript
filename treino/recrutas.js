5793fc5084e9f3764ba3cab90bc54b935be7581d
flavio088 <flavio088gomes@gmail>
2024-01-18 20:52:22 -0300

    Coleta de dados

--- treino/recrutas.js
+++ treino/recrutas.js
@@ -1,3 +1,11 @@
 const primeiroNome = prompt("Informe o primeiro nome ? ")
 const sobrenome = prompt("informe o segundo nome ? ")
 const campoDeEstudo = prompt("Qual é o campo de estudo ? ")
+const anoDeNascimento = prompt("Qual é o ano de nascimento ?")
+
+alert(
+    "Recruta cadastrado com sucesso! \n" +
+    "\nNome completo: " + primeiroNome + " " + sobrenome +
+    "\nCampo de estudo: " + campoDeEstudo +
+    "\nIdade: " + (2023 - anoDeNascimento) 
+)
